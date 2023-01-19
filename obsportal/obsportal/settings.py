18d22b9059604cf6e1e8227c3c98e18471f5d8fa
Moises Castillo <moemyself3@gmail.com>
2023-01-19 16:04:25 -0600

    fix ALLOWED_HOSTS in obsportal.settings

--- obsportal/obsportal/settings.py
+++ obsportal/obsportal/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split()
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split()
 
 # Application definition
 
