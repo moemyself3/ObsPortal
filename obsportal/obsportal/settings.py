47ed5ef8357b262bd05f3dab685a4063bf09e2b3
Moises Castillo <moemyself3@gmail.com>
2023-02-12 13:04:09 -0600

    add routines app and migrations

--- obsportal/obsportal/settings.py
+++ obsportal/obsportal/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'devices.apps.DevicesConfig',
     'pages.apps.PagesConfig',
     'scheduler.apps.SchedulerConfig',
+    'routines.apps.RoutinesConfig',
 ]
 
 MIDDLEWARE = [
--- obsportal/scheduler/models.py
+++ obsportal/scheduler/models.py
@@ -5,5 +5,6 @@ class Event(models.Model):
     event_start_datetime = models.DateTimeField('Start Time')
     event_end_datetime = models.DateTimeField('End Time')
     site = models.ForeignKey('devices.Site', on_delete=models.CASCADE, blank=True)
+    routine = models.ForeignKey('routines.Routine', on_delete=models.CASCADE, blank=True)
     manager = models.CharField(max_length=120) 
     description = models.TextField(blank=True)
