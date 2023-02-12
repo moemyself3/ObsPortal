bfc3a066862b7dcac4bb16ac86d9d6e7ef155079
Moises Castillo <moemyself3@gmail.com>
2023-02-12 12:49:23 -0600

    Add Event Start/End time and split fields

--- obsportal/scheduler/forms.py
+++ obsportal/scheduler/forms.py
@@ -9,7 +9,14 @@ class AddEventForm(forms.ModelForm):
     
         widgets = { 
             'name': forms.CharField.widget(attrs={'class':'form-control'}),
-            'event_date': forms.DateTimeField.widget(attrs={'id':'datetimepicker'}),
+            'event_start_datetime': forms.SplitDateTimeField.widget(
+                date_attrs={'type':'date'}, 
+                time_attrs={'type':'time'}
+                ),
+            'event_end_datetime': forms.SplitDateTimeField.widget(
+                date_attrs={'type':'date'},
+                time_attrs={'type':'time'}
+                ),
             'site': forms.ChoiceField.widget(attrs={'class':'form-control'}),
             'manager': forms.ChoiceField.widget(attrs={'class':'form-control'}),
             'description': forms.CharField.widget(attrs={'class':'form-control'}),
--- obsportal/scheduler/models.py
+++ obsportal/scheduler/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
-    event_date = models.DateTimeField()
+    event_start_datetime = models.DateTimeField('Start Time')
+    event_end_datetime = models.DateTimeField('End Time')
     site = models.ForeignKey('devices.Site', on_delete=models.CASCADE, blank=True)
     manager = models.CharField(max_length=120) 
     description = models.TextField(blank=True)
