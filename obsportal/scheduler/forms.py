1b7ef42d5c1a1287b6c8c8b17d53396691f49091
Moises Castillo <moemyself3@gmail.com>
2023-02-15 17:05:55 -0600

    Add validation to event start datetime

--- obsportal/scheduler/forms.py
+++ obsportal/scheduler/forms.py
@@ -14,6 +14,16 @@ class AddEventForm(forms.ModelForm):
                 date_attrs={'type':'date'},
                 time_attrs={'type':'time'}))
     
+    def clean(self):
+        super(AddEventForm, self).clean()
+
+        start_datetime = self.cleaned_data.get('event_start_datetime')
+        end_datetime = self.cleaned_data.get('event_end_datetime')
+
+        if start_datetime >= end_datetime:
+            self.errors['event_start_datetime'] = self.error_class([
+                'Start time must be before the end time.'])
+   
     class Meta:
         model = Event
         fields = '__all__'
