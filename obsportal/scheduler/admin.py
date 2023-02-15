5ceb5fed25be22445b8a1e70dd91293f44f2510f
Moises Castillo <moemyself3@gmail.com>
2023-02-15 16:24:18 -0600

    Add Category to Event, include in admin, migrations

--- obsportal/scheduler/admin.py
+++ obsportal/scheduler/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Event
+from .models import Event, Category
 
 admin.site.register(Event)
+admin.site.register(Category)
--- obsportal/scheduler/models.py
+++ obsportal/scheduler/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.contrib import admin
 
+class Category(models.Model):
+    name = models.CharField('Category', max_length=50, unique=True)
+   
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+def defaultCategoryId():
+    category_id = Category.objects.get_or_create(name='Uncategorized')[0].pk
+    return category_id
+
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_start_datetime = models.DateTimeField('Start Time')
@@ -9,3 +22,7 @@ class Event(models.Model):
     routine = models.ForeignKey('routines.Routine', on_delete=models.CASCADE, blank=True, null=True)
     manager = models.CharField(max_length=120, blank=True) 
     description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=defaultCategoryId)
+
+    def __str__(self):
+        return self.name + ' | ' + str(self.event_start_datetime)
