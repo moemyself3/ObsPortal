4746de6e07a232213d70ea8d8b5690a73f8578e8
Moises Castillo <moemyself3@gmail.com>
2023-02-21 16:26:22 -0600

    Add clickable dates to scheduler calendar

--- obsportal/scheduler/views.py
+++ obsportal/scheduler/views.py
@@ -11,8 +11,8 @@ from .models import Event, Category
 
 import calendar
 
-from calendar import HTMLCalendar
 from datetime import datetime
+from datetime import date
 
 # Schedule views
 class EventListView(ListView):
@@ -25,24 +25,36 @@ class EventListView(ListView):
 
         #get current datetime
         now = timezone.make_aware(datetime.now())
-        year = now.year
-        month = now.month
-        day = now.day
+        current_year = now.year
+        current_month = now.month
+        current_day = now.day
         
         # Calendar and formatting
-        calendar = HTMLCalendar().formatmonth(year, month)
-        calendar = calendar.replace(
+        current_calendar = calendar.HTMLCalendar().formatmonth(current_year, current_month)
+        current_calendar = current_calendar.replace(
                 '<table ', '<table class="table table-striped table-hover" '
                 )
 
-        calendar = calendar.replace(
+        current_calendar = current_calendar.replace(
                 '<td ', '<td  width="50" height="50"'
                 )
 
-        calendar = calendar.replace(
-                '>'+str(day)+'<',
-                'style="background-color:#FFDD33;">'+str(day)+'<')
-        context['calendar'] = calendar
+        current_calendar = current_calendar.replace(
+                '>'+str(current_day)+'<',
+                'style="background-color:#FFDD33;">'+str(current_day)+'<')
+
+        for day in calendar.Calendar().itermonthdays(year=current_year, month=current_month):
+            if day != 0:
+                calendar_date = date(year=current_year, month=current_month, day=day)
+                current_calendar = current_calendar.replace(
+                        '>' + str(day) + '<',
+                        '><a href=' + reverse_lazy('scheduler:event-lookup') +
+                        '?date='+ str(calendar_date) +
+                        ' class="btn btn-light w-100" role="button" '
+                        'data-bs-toggle="tooltip" data-bs-title="Default tooltip"'
+                        ' >'+ str(day) + '</a><'
+                        )
+        context['calendar'] = current_calendar
         return context
 
 
